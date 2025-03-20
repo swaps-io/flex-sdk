53aaac505c99c094dbe108c423f772f001c3e7b5
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-03-20 13:05:07 +0400

    Add component docs

--- src/component/domain.ts
+++ src/component/domain.ts
@@ -1,10 +1,35 @@
 import { FlexHex, FlexToHexValue, flexConcatHex, flexSliceHex, flexToHex } from '../core';
 
+/**
+ * Parameters for {@link flexAssignComponentDomain} function.
+ *
+ * @category Component
+ */
 export interface FlexAssignComponentDomainParams {
+  /**
+   * Domain value to assign to {@link data} _(8 bytes)_.
+   */
   domain: FlexToHexValue;
+
+  /**
+   * Data to assign {@link} domain to _(32 bytes)_.
+   */
   data: FlexToHexValue;
 }
 
+/**
+ * Assigns specified domain to component data.
+ *
+ * Original data may have zeros or some value for domain - it will be overwritten.
+ *
+ * Domain is assumed to be in most significant part _(8 bytes)_ of component data _(32 bytes)_.
+ *
+ * @param params Function {@link FlexAssignComponentDomainParams | parameters}.
+ *
+ * @returns Component data with new domain _(32 bytes)_.
+ *
+ * @category Component
+ */
 export function flexAssignComponentDomain(params: FlexAssignComponentDomainParams): FlexHex {
   return flexConcatHex([flexToHex(params.domain, 8), flexSliceHex(flexToHex(params.data, 32), 8)]);
 }
--- src/component/hash.ts
+++ src/component/hash.ts
@@ -2,11 +2,36 @@ import { FlexHex, FlexToHexValue, flexCalcHash, flexConcatHex, flexToHex } from 
 
 import { flexAssignComponentDomain } from './domain';
 
+/**
+ * Parameters for {@link flexCalcComponentHash} function.
+ *
+ * @category Component
+ */
 export interface FlexCalcComponentHashParams {
+  /**
+   * Component domain to assign to {@link data} for hash calculation _(8 bytes)_.
+   */
   domain: FlexToHexValue;
+
+  /**
+   * Component data ({@link domain} will be overwritten) _(32 bytes each)_.
+   */
   data: readonly [FlexToHexValue, ...FlexToHexValue[]];
 }
 
+/**
+ * Calculates {@link flexCalcHash | hash} of component data {@link flexAssignComponentDomain | assigning} domain to it
+ * prior.
+ *
+ * Related contracts:
+ * - {@link !FlexEfficientHash | `FlexEfficientHash`}
+ *
+ * @param params Function {@link FlexCalcComponentHashParams | parameters}.
+ *
+ * @returns Domain-aware component data hash.
+ *
+ * @category Component
+ */
 export function flexCalcComponentHash(params: FlexCalcComponentHashParams): FlexHex {
   return flexCalcHash(
     flexConcatHex([
