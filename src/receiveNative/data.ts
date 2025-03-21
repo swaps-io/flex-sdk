b5dc6d626a0f229ba107f6b11509eb654cb8a55f
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-03-21 15:56:39 +0400

    Add receive native docs

--- src/receiveNative/data.ts
+++ src/receiveNative/data.ts
@@ -2,22 +2,93 @@ import { FlexHex, FlexToHexValue } from '../core';
 import { flexCalcReceiveHash, flexEncodeReceiveData0, flexEncodeReceiveData1 } from '../receive';
 import { flexEncodeReceiveFromData0, flexEncodeReceiveFromData1 } from '../receiveFrom';
 
+/**
+ * Parameters for {@link flexEncodeReceiveNativeData} function.
+ *
+ * @category Receive Native
+ */
 export interface FlexEncodeReceiveNativeDataParams {
+  /**
+   * Address of native asset sender (i.e. caller) _(20 bytes)_.
+   */
   sender: FlexToHexValue;
+
+  /**
+   * Address of native asset receiver _(20 bytes)_.
+   */
   receiver: FlexToHexValue;
+
+  /**
+   * Should verify receiver signature as _contract_ one rather than EOA.
+   */
   receiverContract: boolean;
+
+  /**
+   * Should not wrap receiver signed message into EIP-191, i.e. signature is of _raw_ data hash.
+   *
+   * @default false
+   */
   receiverNoMessageWrap?: boolean;
+
+  /**
+   * Should not fallback to receiver signature verification as _contract_ after failed EOA check. In other words,
+   * asserts the signature is known to be EOA and should not be attempted as contract one.
+   *
+   * @default false
+   */
   receiverNoRetryAsContract?: boolean;
+
+  /**
+   * Amount of native asset to receive _(32 bytes)_.
+   */
   amount: FlexToHexValue;
+
+  /**
+   * Deadline of receive native operation, i.e. time after which attempt to call receive operation will fail
+   * _(6 bytes)_.
+   *
+   * Should not exceed {@link FLEX_MAX_RECEIVE_DEADLINE | max supported value}.
+   */
   deadline: FlexToHexValue;
+
+  /**
+   * Nonce of receive native operation selected by {@link receiver} _(6 bytes)_.
+   */
   nonce: FlexToHexValue;
 }
 
+/**
+ * Receive native {@link flexEncodeReceiveNativeData | encoded} data.
+ *
+ * @category Receive Native
+ */
 export interface FlexReceiveNativeData {
+  /**
+   * Receive core part of the data _(32 bytes each)_.
+   */
   receiveData: [FlexHex, FlexHex];
+
+  /**
+   * Receive from part of the data _(32 bytes each)_.
+   */
   receiveFromData: [FlexHex, FlexHex];
 }
 
+/**
+ * Encodes data for receive native call.
+ *
+ * Receive native call is performed by _sender_ using _receiver_ signature, passing native amount as call `msg.value`.
+ *
+ * Related contracts:
+ * - {@link !FlexReceiveNativeFacet | `FlexReceiveNativeFacet`}
+ * - {@link !IFlexReceiveNative | `IFlexReceiveNative`}
+ *
+ * @param params Function {@link FlexEncodeReceiveNativeDataParams | parameters}.
+ *
+ * @returns Receive native {@link FlexReceiveNativeData | data}.
+ *
+ * @category Receive Native
+ */
 export function flexEncodeReceiveNativeData(params: FlexEncodeReceiveNativeDataParams): FlexReceiveNativeData {
   const receiveData: FlexReceiveNativeData['receiveData'] = [
     flexEncodeReceiveData0({
--- src/receiveNative/hash.ts
+++ src/receiveNative/hash.ts
@@ -3,11 +3,36 @@ import { flexCalcReceiveFromHash } from '../receiveFrom';
 
 import { FlexReceiveNativeData } from './data';
 
+/**
+ * Parameters for {@link flexCalcReceiveNativeHash} function.
+ *
+ * @category Receive Native
+ */
 export interface FlexCalcReceiveNativeHashParams {
+  /**
+   * Receive native domain to calculate component hash for _(8 bytes)_.
+   */
   domain: FlexToHexValue;
+
+  /**
+   * Receive native component data to calculate hash of _(32 bytes each)_.
+   */
   data: Pick<FlexReceiveNativeData, 'receiveFromData'>;
 }
 
+/**
+ * Calculates hash of receive native component {@link flexEncodeReceiveNativeData | data}.
+ *
+ * Related contracts:
+ * - {@link !FlexReceiveNativeFacet | `FlexReceiveNativeFacet`}
+ * - {@link !IFlexReceiveNative | `IFlexReceiveNative`}
+ *
+ * @param params Function {@link FlexCalcReceiveNativeHashParams | parameters}.
+ *
+ * @returns Receive native component data hash _(32 bytes)_.
+ *
+ * @category Receive Native
+ */
 export function flexCalcReceiveNativeHash(params: FlexCalcReceiveNativeHashParams): FlexHex {
   return flexCalcReceiveFromHash({ domain: params.domain, data: params.data.receiveFromData });
 }
