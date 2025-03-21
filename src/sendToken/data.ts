e7e5c6ef10f6fd59fc072fea8d7037417fc60e59
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-03-21 17:33:22 +0400

    Add send token docs

--- src/sendToken/data.ts
+++ src/sendToken/data.ts
@@ -1,20 +1,86 @@
 import { FlexHex, FlexToHexValue } from '../core';
 import { flexEncodeSendData0, flexEncodeSendData1, flexEncodeSendData2, flexEncodeSendData3 } from '../send';
 
+/**
+ * Parameters for {@link flexEncodeSendTokenData} function.
+ *
+ * @category Send Token
+ */
 export interface FlexEncodeSendTokenDataParams {
+  /**
+   * Address of token asset sender (i.e. caller) _(20 bytes)_.
+   */
   sender: FlexToHexValue;
+
+  /**
+   * Address of token asset receiver _(20 bytes)_.
+   */
   receiver: FlexToHexValue;
+
+  /**
+   * Address of ERC-20 token to perform send of _(20 bytes)_.
+   */
   token: FlexToHexValue;
+
+  /**
+   * Amount of token asset to send _(32 bytes)_.
+   */
   amount: FlexToHexValue;
+
+  /**
+   * Start of send token operation _(6 bytes)_.
+   *
+   * Send operation cannot be performed earlier than this time. Also serves for controlling send operations
+   * _chronological_ order.
+   */
   start: FlexToHexValue;
+
+  /**
+   * Duration of send token operation _(6 bytes)_.
+   *
+   * Forms _deadline_ when added to {@link start}. Attempt to perform send operation after the deadline will fail.
+   */
   duration: FlexToHexValue;
+
+  /**
+   * Send group index selected by the {@link sender} _(6 bytes)_.
+   */
   group: FlexToHexValue;
 }
 
+/**
+ * Send token {@link flexEncodeSendTokenData | encoded} data.
+ *
+ * @category Send Token
+ */
 export interface FlexSendTokenData {
+  /**
+   * Send token data _(32 bytes each)_.
+   */
   sendData: [FlexHex, FlexHex, FlexHex, FlexHex];
 }
 
+/**
+ * Encodes data for send token call.
+ *
+ * Send token call is performed by _sender_. ERC-20 token asset is transferred from _sender_ account to _receiver_
+ * during the call. This spends corresponding allowance, which should be granted by _sender_ in advance.
+ *
+ * Send token operation can be used as requirement for transiting {@link FLEX_RECEIVE_STATE_RECEIVED | "received"}
+ * state to {@link FLEX_RECEIVE_STATE_CONFIRMED | "confirmed"} in other network.
+ *
+ * Related contracts:
+ * - {@link !FlexSendTokenFacet | `FlexSendTokenFacet`}
+ * - {@link !IFlexSendToken | `IFlexSendToken`}
+ * - {@link !FlexSendTokenDomainFacet | `FlexSendTokenDomainFacet`}
+ * - {@link !IFlexSendTokenDomain | `IFlexSendTokenDomain`}
+ *
+ * @param params Function {@link FlexEncodeSendTokenDataParams | parameters}.
+ *
+ * @returns Send token {@link FlexSendTokenData | data}.
+ *
+ * @category Send Token
+ */
 export function flexEncodeSendTokenData(params: FlexEncodeSendTokenDataParams): FlexSendTokenData {
   const sendData: FlexSendTokenData['sendData'] = [
     flexEncodeSendData0({
--- src/sendToken/hash.ts
+++ src/sendToken/hash.ts
@@ -3,11 +3,38 @@ import { flexCalcSendHash } from '../send';
 
 import { FlexSendTokenData } from './data';
 
+/**
+ * Parameters for {@link flexCalcSendTokenHash} function.
+ *
+ * @category Send Token
+ */
 export interface FlexCalcSendTokenHashParams {
+  /**
+   * Send token domain to calculate component hash for _(8 bytes)_.
+   */
   domain: FlexToHexValue;
+
+  /**
+   * Send token component data to calculate hash of.
+   */
   data: Pick<FlexSendTokenData, 'sendData'>;
 }
 
+/**
+ * Calculates hash of receive token component {@link flexEncodeSendTokenData | data}.
+ *
+ * Related contracts:
+ * - {@link !FlexSendTokenFacet | `FlexSendTokenFacet`}
+ * - {@link !IFlexSendToken | `IFlexSendToken`}
+ * - {@link !FlexSendTokenDomainFacet | `FlexSendTokenDomainFacet`}
+ * - {@link !IFlexSendTokenDomain | `IFlexSendTokenDomain`}
+ *
+ * @param params Function {@link FlexCalcSendTokenHashParams | parameters}.
+ *
+ * @returns Send token component data hash _(32 bytes)_.
+ *
+ * @category Send Token
+ */
 export function flexCalcSendTokenHash(params: FlexCalcSendTokenHashParams): FlexHex {
   return flexCalcSendHash({ domain: params.domain, data: params.data.sendData });
 }
